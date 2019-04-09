671d73c6054a2aa9941efb66ed8fabad3b1eb569
stephaniebang <ej.bang27@gmail.com>
2019-04-08 23:07:39 -0300

    Use ggplot function , save plots and refactor code

--- resultsAnalysis.R
+++ resultsAnalysis.R
@@ -12,71 +12,106 @@ files <- dir(path)
 
 ## TESTS WITH ARRAY SIZE --------------------------------------------------
 
-t4f1 <- read.table(paste0(path, files[1]), header=T, sep=';')
+t_array <- read.table(paste0(path, files[1]), header=T, sep=';')
 
-# View table and plot
-View(t4f1)
-qplot(t4f1$array_size, t4f1$time, main='4 threads and 1 if', xlab='Array size', ylab='Time (s)')
+# view table and plot
+View(t_array)
 
-m <- mean(t4f1$time)
-s <- sd(t4f1$time)
+ggplot(t_array, aes(t_array$array_size, t_array$time)) +
+  geom_point() +
+  geom_smooth(method="lm", se=T) +
+  labs(
+    title="Array size variation",
+    subtitle="4 threads and 1 if",
+    y="Time (s)",
+    x="Array size"
+  )
+
+# save plot
+ggsave("results/arrayVariation.png")
+
+# get statistics
+m <- mean(t_array$time)
+s <- sd(t_array$time)
 
 # ci for 95%
 error <- qnorm(0.975)*s/sqrt(100)
 
 # print results
 m
-min(t4f1$time)
-max(t4f1$time)
+min(t_array$time)
+max(t_array$time)
+s
 m-error
 m+error
 
 
 ## TESTS WITH NUMBER OF THREADS -------------------------------------------
 
-i1a100000 <- read.table(paste0(path, files[2]), header=T, sep=';')
+t_threads <- read.table(paste0(path, files[2]), header=T, sep=';')
 
-# View table and plot
-View(i1a100000)
-qplot(i1a100000$threads, i1a100000$time, main='array size of 1.000.000 and 1 if', xlab='Number of threads', ylab='Time (s)')
+# view table and plot
+View(t_threads)
 
-times <- i1a100000$time
-m <- mean(times)
-s <- sd(times)
+ggplot(t_threads, aes(t_threads$threads, t_threads$time)) +
+  geom_point() +
+  geom_smooth(method="lm", se=T) +
+  labs(
+    title="Threads number variation", subtitle="Array size of 1.000.000 and 1 if", y="Time (s)", x="Number of threads")
+
+# save plot
+ggsave("results/threadsVariation.png")
+
+# get statistics
+m <- mean(t_threads$time)
+s <- sd(t_threads$time)
 
 # ci for 95%
 error <- qnorm(0.975)*s/sqrt(100)
 
 # print results
 m
-min(times)
-max(times)
+min(t_threads$time)
+max(t_threads$time)
+s
 m-error
 m+error
 
 
-
 ## TESTS WITH IF CHAIN SIZE -----------------------------------------------
 
-t4a100000 <- read.table(paste0(path, files[3]), header=T, sep=';')
+t_ifs <- read.table(paste0(path, files[3]), header=T, sep=';')
 
 # Add ifs column
-t4a100000$ifs <- c(1:100)
+t_ifs$ifs <- c(0:100)
 
 # View table and plot
-View(t4a100000)
-qplot(t4a100000$ifs, t4a100000$time, main='4 threads and array size of 1.000.000', xlab='Number of if\'s', ylab='Time (s)')
+View(t_ifs)
 
-times <- t4a100000$time
-m <- mean(times)
-s <- sd(times)
+ggplot(t_ifs, aes(t_ifs$ifs, t_ifs$time)) +
+  geom_point() +
+  geom_smooth(method="lm", se=T) +
+  labs(
+    title="IF chain size variation",
+    subtitle="4 threads and array size of 1.000.000",
+    y="Time (s)",
+    x="Size of IF chain size"
+  )
+
+# save plot
+ggsave("results/ifsVariation.png")
+
+# get statistics
+m <- mean(t_ifs$time)
+s <- sd(t_ifs$time)
 
 # ci for 95%
 error <- qnorm(0.975)*s/sqrt(100)
 
 # print results
 m
-min(times)
-max(times)
+min(t_ifs$time)
+max(t_ifs$time)
+s
 m-error
 m+error
